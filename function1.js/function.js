6539ac334d27e016502c6971067d97f8d7a10454
isisgoncalves <isisvow@gmail.com>
2024-04-17 15:57:58 -0300

    End

---
+++ function1.js/function.js
@@ -0,0 +1,8 @@
+// Vamos criar uma função simples para que possa ser executada. Ao ser executada esta primeira função possa realizar o calculo - a partir de um argumento numérico - o valor de uma porcentagem que também será oferecido como segundo argumento dessa função.
+
+function calculo(a, b){
+  return (a * b) / 100
+}
+let porcentagem =  calculo(10, 300)
+
+alert(porcentagem)
